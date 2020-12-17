b84b27f94d01757712a9426600692d51e121d401
interactiveninja <gabrielnadolny2@gmail.com>
2020-12-17 14:53:48 +0100

    added image support

--- index.js
+++ index.js
@@ -9,51 +9,70 @@ const fs = require('fs')
 const Handlebars = require("handlebars");
 var bodyParser = require('body-parser')
 const QRCode = require('qrcode')
+const request = require('request');
+const sharp = require('sharp')
 
+const FILENAME = path.join(__dirname + "/image.png")
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 app.use(bodyParser.json())
- 
+
 
 app.get("/", (req, res) => {
 
-  res.sendFile(path.join(__dirname + "/image.png"))
+  res.sendFile(FILENAME)
 })
 
-app.get('/set',(_,res) => {
+app.get('/set', (_, res) => {
 
   res.sendFile(path.join(__dirname + "/form.html"))
 
 })
 
-app.post('/set',(req,res) =>{
+app.post('/set', (req, res) => {
 
   let val = req.body.text
   let typ = req.body.typ
   let html = Handlebars.compile(fs.readFileSync(path.join(__dirname + "/text.html"), "utf-8"))
-  if(val == "ninja") {
-    fs.copyFileSync(path.join(__dirname + "/error.png"),path.join(__dirname + "/image.png"))
+  if (val == "ninja") {
+    fs.copyFileSync(path.join(__dirname + "/error.png"), FILENAME)
     return
 
   }
   switch (typ) {
     case "qr":
-      QRCode.toFile(path.join(__dirname + "/image.png"),val).then(() =>{
+      QRCode.toFile(FILENAME, val).then(() => {
         res.redirect("/")
       })
       break;
     case "text":
       nodeHtmlToImage({
         output: './image.png',
-        html: html({text:val})
+        html: html({ text: val })
       }).then(() => {
-      res.redirect("/")
+        res.redirect("/")
+      });
+      break;
+    case "img":
+      download(val, FILENAME+'1', function (err) {
+        sharp(FILENAME+'1').resize({ height: 300, width: 400 }).toFile(FILENAME)
+          .then(function (FILENAME) {
+            // fs.unlinkSync(path.join(FILENAME + "1"))
+            res.redirect("/")
+          }).catch(function (err) {
+            console.log(err);
+          });
       });
       break;
   }
-  
+
 })
 
+let download = (uri, filename, callback) => {
+  request.head(uri, (err, res, body) => {
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
 
 
 app.listen(PORT, () => {
