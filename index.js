d3ddabd9ac6bc0cf0363fba47a4fc971931121f9
InteractiveNinja <gabrielnadolny2@gmail.com>
2020-12-17 13:33:33 +0100

    idk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 
 
 app.get("/", (req, res) => {
@@ -8,15 +8,24 @@ app.get("/", (req, res) => {
 
     var figlet = require('figlet');
 
-    figlet('Hello World!!', function (err, data) {
-
-        const textToImage = require('text-to-image');
-        textToImage.generate(data).then(function (dataUri) {
-            res.send(`<img src='${dataUri}'>`)
-            // res.send(`<pre>${data}</pre>`)
-        });
+    figlet.text('Hello World', function (err, data) {
+        const nodeHtmlToImage = require('node-html-to-image')
+        nodeHtmlToImage({
+            output: './image.png',
+            html: `<html><head>
+            <style>
+              body {
+                width: 400px;
+                height: 300px;
+              }
+            </style>
+          </head><body><pre>${data}</pre></body></html>`//,
+            // transparent: true
+        }).then(() =>{
+    res.sendFile(path.join(__dirname + "/image.png"))
+            
+        })
     });
-
 })
 
 
