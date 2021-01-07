d2fea97447610decdbc8b7d1c47a0d85c7190b56
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-01-07 09:51:53 +0100

    some refactoring

--- index.js
+++ index.js
@@ -7,82 +7,116 @@ const config = new conf.config(path.join(__dirname + "/config.json"))
 const PORT = config.get('port')
 const fs = require('fs')
 const Handlebars = require("handlebars");
-var bodyParser = require('body-parser')
 const QRCode = require('qrcode')
-const request = require('request');
-const sharp = require('sharp')
+const bodyparser = require('body-parser')
 
-const FILENAME = path.join(__dirname + "/image.png")
-app.use(bodyParser.urlencoded({ extended: false }))
+const FILENAME = path.join(__dirname + "/render/image.png")
+const templatePath = path.join(__dirname + "/templates/")
 
-app.use(bodyParser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
 
+app.use(bodyparser.json())
 
 app.get("/", (req, res) => {
 
-  res.sendFile(FILENAME)
+    res.sendFile(FILENAME)
+})
+app.get("/good", (req, res) => {
+
+    res.sendFile(templatePath + "/good.html")
+})
+app.get("/error", (req, res) => {
+
+    res.sendFile(templatePath + "/error.html")
+})
+app.get("/error-pin", (req, res) => {
+
+    res.sendFile(templatePath + "/error-pin.html")
 })
 
-app.get('/set', (_, res) => {
+app.get('/set', (req, res) => {
 
-  res.sendFile(path.join(__dirname + "/form.html"))
+    res.sendFile(templatePath + "form.html")
 
 })
+
 
 app.post('/set', (req, res) => {
 
-  let val = req.body.text
-  let typ = req.body.typ
-  let pw = req.body.pw
-  
-  if(pw != config.get("pw")) {
-    res.redirect("/")
-    return 
-  }
-  let html = Handlebars.compile(fs.readFileSync(path.join(__dirname + "/text.html"), "utf-8"))
-  if (val == "ninja") {
-    fs.copyFileSync(path.join(__dirname + "/error.png"), FILENAME)
-    return
+    // Variabeln
+    let val = req.body.text
+    let typ = req.body.typ
+    let pw = req.body.pw
 
-  }
-  switch (typ) {
-    case "qr":
-      QRCode.toFile(FILENAME, val).then(() => {
-        res.redirect("/")
-      })
-      break;
-    case "text":
-      nodeHtmlToImage({
-        output: './image.png',
-        html: html({ text: val })
-      }).then(() => {
-        res.redirect("/")
-      });
-      break;
-    case "img":
-      download(val, FILENAME+'1', function (err) {
-        sharp(FILENAME+'1').resize({ height: 300, width: 400 }).toFile(FILENAME)
-          .then(function (FILENAME) {
-            // fs.unlinkSync(path.join(FILENAME + "1"))
-            res.redirect("/")
-          }).catch(function (err) {
-            console.log(err);
-          });
-      });
-      break;
-  }
+    //Pin Prüfung
+    if (pw != config.get("pw")) {
+        res.redirect("/error-pin")
+        return
+    }
+
+    switch (typ) {
+        case "qr":
+            createQR(val, (err) => {
+                if (err) {
+                    res.redirect("error")
+                    return
+                }
+                res.redirect("/good")
+            })
+            break;
+        case "text":
+            createText(val, (err) => {
+                if (err) {
+                    res.redirect("/error")
+                    return
+                }
+                res.redirect("/good")
+            })
+
+            break;
+        case "img":
+            createImage(val, (err) => {
+                if (err) {
+                    res.redirect("/error")
+                    return
+                }
+                res.redirect("/good")
+            })
+            break;
+    }
 
 })
 
-let download = (uri, filename, callback) => {
-  request.head(uri, (err, res, body) => {
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
+
+let createQR = (val, callback) => {
+    QRCode.toFile(FILENAME, val, (err) => {
+        callback(err)
+    })
+
+}
+
+let createText = (val, callback) => {
+    let html = Handlebars.compile(fs.readFileSync(path.join(__dirname + "/templates/text.html"), "utf-8"))
+    nodeHtmlToImage({
+        output: './render/image.png',
+        html: html({ text: val })
+    }).then(() => {
+        callback(false)
+    });
+}
+let createImage = (val, callback) => {
+    let html = Handlebars.compile(fs.readFileSync(path.join(__dirname + "/templates/img.html"), "utf-8"))
+    nodeHtmlToImage({
+        output: './render/image.png',
+        html: html({ imgurl: val })
+    }).then(() => {
+        callback(false)
+    });
+}
 
 
 app.listen(PORT, () => {
 
-  console.log(`Server läuft auf Port ${PORT}`)
+    console.log(`Server läuft auf Port ${PORT}`)
 
 })
