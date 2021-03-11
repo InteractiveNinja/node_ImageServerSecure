7987caccaa7aedc7deadee95e649eb22f462abf8
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-03-11 17:00:28 +0100

    fixed the code

--- src/imageGenerator.ts
+++ src/imageGenerator.ts
@@ -1,39 +1,52 @@
-import nodeHtmlToImage, {} from 'node-html-to-image'
+import nodeHtmlToImage, { } from 'node-html-to-image'
 import { toFile } from 'qrcode'
-import {join} from 'path'
+import { join } from 'path'
 import { readFileSync } from 'fs'
-import {compile} from 'handlebars'
+import { compile } from 'handlebars'
 
 
 
 
-const FILENAME = join(__dirname + "/render/image.png")
+const FILENAME = "public/render/image.png"
 
 
-export let createQR = (val: any, callback: { (err: any): void; (arg0: any): void }) => {
-    toFile(FILENAME, val, (err: any) => {
-        callback(err)
+export let createQR = (val: string): Promise<void> => {
+    console.log("image create anfrage")
+
+    return new Promise((res, rej) => {
+        if (val == undefined) rej()
+
+        toFile(FILENAME, val, (err: any) => {
+            if (!err) {
+
+                res()
+            }
+            rej()
+        })
     })
 
+
 }
 
-export let createText = (val: any, callback: { (err: any): void; (arg0: boolean): void }) => {
-    let html = compile(readFileSync(join(__dirname + "/templates/text.html"), "utf-8"))
-    nodeHtmlToImage({
-        output: join(__dirname + "/render/image.png"),
-        html: html({ text: val })
-    }).then(() => {
-        callback(false)
-    });
+export let createText = (val: string) : Promise<void> => {
+    return new Promise((res,rej) =>{
+        nodeHtmlToImage({
+            output: "public/render/image.png",
+            html: readFileSync("templates/text.html", "utf-8"),
+            content: {text : val}
+        }).then(() => res()).catch((e) => console.log(e));
+    })
+    
 }
-export let createImage = (val: any, callback: { (err: any): void; (arg0: boolean): void }) => {
-    let html = compile(readFileSync(join(__dirname + "/templates/img.html"), "utf-8"))
-    nodeHtmlToImage({
-        output: join(__dirname + "/render/image.png"),
-        html: html({ imgurl: val })
-    }).then(() => {
-        callback(false)
-    });
+export let createImage = (val: string): Promise<void> => {
+    return new Promise((res,rej) =>{
+
+        nodeHtmlToImage({
+            output: "public/render/image.png",
+            html: readFileSync("templates/img.html", "utf-8"),
+            content: {imgurl: val}
+        }).then(() => res()).catch(() => rej());
+    })
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import {createServer} from 'https'
+import { createServer } from 'https'
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
-import {config} from '@interactiveninja/config-reader'
+import { config } from '@interactiveninja/config-reader'
 import { readFileSync } from 'fs'
-
+import { createHash } from 'crypto'
+import { createImage, createQR, createText } from './imageGenerator'
 const app = express()
 const configManager = config("config.json")
 
@@ -13,7 +14,7 @@ const PORT = configManager.get('port')
 
 const FILENAME = "public/render/image.png"
 const templatePath = "templates"
-const sendFileOptions = {"root":"."}
+const sendFileOptions = { "root": "." }
 
 // HTTPS Certificate
 const privateKey = readFileSync("cert/privkey.pem");
@@ -29,17 +30,23 @@ app.use(json())
 
 app.get("/", (req, res) => {
 
-    res.sendFile(FILENAME,sendFileOptions)
+    res.sendFile(FILENAME, sendFileOptions)
 })
 
 app.get("/show", (req, res) => {
 
-    res.sendFile(templatePath + "/show.html",sendFileOptions)
+    res.sendFile(templatePath + "/show.html", sendFileOptions)
 })
 
 app.get('/set', (req, res) => {
 
-    res.sendFile(templatePath + "/form.html",sendFileOptions)
+    res.sendFile(templatePath + "/form.html", sendFileOptions)
+
+})
+
+app.get('/error', (req, res) => {
+
+    res.sendFile(templatePath + "/error.html", sendFileOptions)
 
 })
 
@@ -47,17 +54,64 @@ app.get('/set', (req, res) => {
 app.post('/set', (req, res) => {
 
     // Variabeln
-    let val =  req.body.text 
-    let typ =  req.body.typ
+    let val = req.body.text
+    let typ = req.body.typ
     let pw = req.body.pw
 
-    if(val != undefined || typ !=  undefined || pw != undefined) {
-        res.sendStatus(200)
+    if (val != undefined || typ != undefined || pw != undefined) {
+
+        checkPW(pw).then(() => {
+            launchMethode(typ, val).then(() => res.redirect("/show")).catch(() => res.redirect("/error"))
+
+        }).catch(() => res.redirect("/error"))
     }
 
 
-    
+
+
+
+
+
+
 })
+
+let launchMethode = (type: string, text: string): Promise<void> => {
+    return new Promise((res, rej) => {
+
+        switch (type) {
+            case "qr":
+                createQR(text).then(() => res()).catch(() => rej())
+                break;
+
+            case "img":
+
+                createImage(text).then(() => res()).catch(() => rej())
+
+                break;
+
+            case "text":
+                createText(text).then(() => res()).catch(() => rej())
+
+                break;
+        }
+
+
+
+
+    })
+
+}
+
+
+let checkPW = (pw: string): Promise<void> => {
+    return new Promise((res, rej) => {
+        if (createHash("md5").update(pw, "utf-8").digest("hex") == configManager.get("pw")) {
+            res();
+        } else {
+            rej();
+        }
+    })
+}
 
 
 createServer({
